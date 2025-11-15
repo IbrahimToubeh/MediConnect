2706245575b20d0ea0d5522d5a8722956c94445d
abdulrahman-alsnour <abdnsour01@gmail.com>
2025-11-15 13:33:03 +0300

    cheak all files

--- src/main/java/com/MediConnect/ai/config/AiConfiguration.java
+++ src/main/java/com/MediConnect/ai/config/AiConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
+//test
 /**
  * Dedicated HTTP client for talking to the OpenAI API.
  * <p>
