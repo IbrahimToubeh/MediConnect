43dcd8c7cf668ff93ac4d8d37b6d7d0da1b7b680
abdulrahman-alsnour <abdnsour01@gmail.com>
2025-11-15 13:27:46 +0300

    test commit

--- src/main/java/com/MediConnect/MediConnectApplication.java
+++ src/main/java/com/MediConnect/MediConnectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+//comment testing
 @SpringBootApplication(scanBasePackages = "com.MediConnect")
 @EnableCaching
 @EnableScheduling
